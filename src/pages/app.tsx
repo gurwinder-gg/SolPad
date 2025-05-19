1f22b16876ad059eb2eca648aa55b9a7260537be
gurwinder-gg <gurwindergagan07@gmail.com>
2025-05-19 11:23:59 -0700

    Update app.tsx

--- src/pages/app.tsx
+++ src/pages/app.tsx
@@ -1,13 +1,17 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+
 import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
 import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
 import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets';
+import { useMemo } from 'react';
 
-const endpoint = process.env.NEXT_PUBLIC_RPC!;
-const wallets = [new PhantomWalletAdapter()];
+require('@solana/wallet-adapter-react-ui/styles.css');
 
 export default function App({ Component, pageProps }: AppProps) {
+  const endpoint = process.env.NEXT_PUBLIC_RPC!;
+  const wallets = useMemo(() => [new PhantomWalletAdapter()], []);
+
   return (
     <ConnectionProvider endpoint={endpoint}>
       <WalletProvider wallets={wallets} autoConnect>
