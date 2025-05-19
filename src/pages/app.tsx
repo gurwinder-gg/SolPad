9a6acb1476f4a389b7fa3ba69674014462ddbf66
gurwinder-gg <gurwindergagan07@gmail.com>
2025-05-19 11:07:12 -0700

    Create app.tsx

---
+++ src/pages/app.tsx
@@ -0,0 +1,20 @@
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
+import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets';
+
+const endpoint = process.env.NEXT_PUBLIC_RPC!;
+const wallets = [new PhantomWalletAdapter()];
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>
+          <Component {...pageProps} />
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+}
