1fedc9f783f0a4676306d0ededf8c07876f1b799
gurwinder-gg <gurwindergagan07@gmail.com>
2025-05-28 07:10:10 -0700

    Update app.tsx

--- src/pages/app.tsx
+++ src/pages/app.tsx
@@ -9,7 +9,7 @@ require('@solana/wallet-adapter-react-ui/styles.css');
 
 export default function App({ Component, pageProps }: AppProps) {
   const endpoint = process.env.NEXT_PUBLIC_RPC!;
-  const wallets = useMemo(() => [new PhantomWalletAdapter()], []);
+  const wallets = useMemo(() => [new PhantomWalletAdapter()], []);   
 
   return (
     <ConnectionProvider endpoint={endpoint}>
